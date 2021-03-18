192dd02956abb3c52686191746d0f8efb58d0994
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2021-03-18 15:06:41 -0500

    Versión final

--- Cliente.py
+++ Cliente.py
@@ -4,49 +4,48 @@ import hashlib
 
 
 
-# Puerto
+# Puerto inicial, cada thread cliente estara en un puerto diferente
+# Desde el 4001 hasta la cantidad de conexiones esperadas sumando de 1 en 1
 port = 4000
 
-# Estipula la direccion del servidor
-print("Escriba la direccion ip del servirdor")
+# Pide la direccion del servidor por consola
+print("Escriba la direccion ip del servirdor (Ej. 127.0.0.1)")
 ip = input()
 
+# Pide la cantidad de clientes por consola
+print("Escriba la cantidad de clientes (Ej. 25)")
+cantClientes = int(input())
 
-
-
-# Estipula la cantidad de clientes
-while True:
-    print("Escriba la cantidad de clientes, minimo 25")
-    cantClientes = int(input())
-    if (cantClientes > 0):
-        break
-
+# Crea una variable que contara la cantidad de clientes creados
 clientesActuales = 0
 
-
-
-
+# Funcion que ejecutaran los threads, donde ocurre la conexion, la recepcion y la confirmacion
 def recibir (num):
-    # Crear el socket
+    # Crea el socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     direccionServidor = (ip, port+num)
+    # Se conecta al servidor
     s.connect(direccionServidor)
 
+    # Recibe el hash esperado del archivo
     hash = s.recv(1024).decode("utf-8")
     s.send(b"Hash Recibido")
-    print("Hash Recibido")
 
+    # Recibe el tamano esperado del archivo
     tamEsperado = int(s.recv(1024).decode("utf-8"))
     s.send(b"Tam Recibido")
-    print("tam: " + str(tamEsperado))
 
     # Crea un archivo para guardar la descarga
-    filename = "ArchivosRecibidos/re"+str(num)+".txt"
+    filename = "ArchivosRecibidos/Cliente"+str(num)+"-Prueba-"+str(cantClientes)+".txt"
     file = open(filename, 'wb')
 
-    # Guarda la descarga por chunks
+    # Variable que cuenta el tamano del archivo que se va recibiendo
     tam = 0
+    # Contador para los paquetes que se recibieron
+    contador = 0
+    # Guarda la descarga por chunks de 512B hasta que el tamano en Bytes recibido sea igual al esperado
     while True:
+        contador += 1
         data = s.recv(512)
         tam += len(data)
         if (tam == tamEsperado):
@@ -60,16 +59,27 @@ def recibir (num):
     file_data = file.read()
     m = hashlib.sha256()
     m.update(file_data)
+    # Calcula el hash del archivo recibido
     hash2 = m.hexdigest()
+
+    # Confirma que sean inguales los hash y envia la confirmacion de si es correcto o no
     if(hash == hash2):
         print("El hash es igual" + " - " +"Hash recibido: " + str(hash) + " - " + "Hash calculado: " + str(hash2) )
-        s.send(b"Archivo Recibido y verificado")
+        s.send(("Archivo Recibido y verificado").encode("utf-8"))
+
     else:
         print("El hash esta mal" + " - " + "Hash recibido: " + str(hash) + " - " + "Hash calculado: " + str(hash2))
+        s.send(("Archivo Recibido y con fallo ").encode("utf-8"))
 
+    # Envia la cantidad de paquetes recibidos
+    s.send(str(contador).encode("utf-8"))
 
-# Crea todos lo clientes
+    # Cierra la conexion con el servidor
+    s.close()
 
+# Loop que crea y ejecuta tantos cleintes como fueron indicados por consola
+# Les pasa por parametro un id de cliente
+print("Esperando para recibir...")
 while clientesActuales<cantClientes:
     clientesActuales +=1
     threading.Thread(target=recibir, args=(clientesActuales,)).start()
